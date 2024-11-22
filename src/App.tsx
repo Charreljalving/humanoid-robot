15fa06fe63195ec04d1e0e2880612b250eeb99cb
Charrel Jalving <61003309+Charreljalving@users.noreply.github.com>
2024-11-22 11:00:51 +0100

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
@@ -8,26 +9,30 @@ import { Companies } from './pages/Companies';
 import { About } from './pages/About';
 import { Articles } from './pages/Articles';
 import { Article } from './pages/Article';
+import { Login } from './pages/Login';
+import { Admin } from './pages/Admin';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white text-neutral-900 flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/robots" element={<Robots />} />
-            <Route path="/companies" element={<Companies />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/articles" element={<Articles />} />
-            <Route path="/articles/:id" element={<Article />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-white text-neutral-900 flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/robots" element={<Robots />} />
+              <Route path="/bedrijven" element={<Companies />} />
+              <Route path="/over-ons" element={<About />} />
+              <Route path="/artikelen" element={<Articles />} />
+              <Route path="/artikelen/:id" element={<Article />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
-
-export default App;

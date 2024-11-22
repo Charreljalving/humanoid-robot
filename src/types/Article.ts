5c89a40928a39a347e277176bd15b3081a680105
Charrel Jalving <61003309+Charreljalving@users.noreply.github.com>
2024-11-22 11:57:46 +0100

    Update src/types/Article.ts [BoltSync]

---
+++ src/types/Article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+  id?: string;
+  titel: string;
+  excerpt: string;
+  inhoud: string;
+  auteur: string;
+  datum: Date;
+  afbeelding: string;
+  categorie: string;
+}
